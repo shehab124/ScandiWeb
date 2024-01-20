c2caa095ce32858df370893e89b9ec755413aab3
shehab124 <chehab.gamal@gmail.com>
2024-01-20 20:10:29 +0200

    Cancel button

--- frontend/src/components/NavbarAdd.jsx
+++ frontend/src/components/NavbarAdd.jsx
@@ -1,13 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = ({ submitHandler, selectProductType }) => {
+
+    const navigate = new useNavigate();
+
+    const handleCancel = () => navigate('/');
+
+
     return (
         <nav>
             <div className="navbar">
                 <h1>Product Add</h1>
                 <div className='links'>
                     <button onClick={submitHandler} disabled={!selectProductType} style={{ color: 'white', backgroundColor: 'black', borderRadius: '8px' }}>Save</button>
-                    <button style={{ color: 'white', backgroundColor: 'black', borderRadius: '8px' }}>Cancel</button>
+                    <button
+                        style={{ color: 'white', backgroundColor: 'black', borderRadius: '8px' }}
+                        onClick={handleCancel}
+                    >
+                        Cancel</button>
                 </div>
             </div>
         </nav>
