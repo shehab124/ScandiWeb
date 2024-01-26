752d83b658cb8781752c678aafba00353b920c0f
shehab124 <chehab.gamal@gmail.com>
2024-01-26 17:53:34 +0200

    Merged config file with Database file

--- backend/models/Database.php
+++ backend/models/Database.php
@@ -6,7 +6,16 @@ class Database
 
     public function __construct()
     {
-        $this->conn = require_once 'config/DBConfig.php';
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'chehab');
+        define('DB_PASSWORD', 'admin');
+        define('DB_NAME', 'scandiweb');
+
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
     }
 
     public function getConnection()
