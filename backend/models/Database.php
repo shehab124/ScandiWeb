5fe5744bc41acc76762f4f27b7bef2421d2b66fa
shehab124 <chehab.gamal@gmail.com>
2024-01-26 17:57:06 +0200

    deleted config.php

--- backend/models/Database.php
+++ backend/models/Database.php
@@ -6,7 +6,16 @@ class Database
 
     public function __construct()
     {
-        $this->conn = require_once 'config/DBConfig.php';
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'u726756365_chehab');
+        define('DB_PASSWORD', 'Jars_Summer2023');
+        define('DB_NAME', 'u726756365_scandiweb');
+
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
     }
 
     public function getConnection()
